d8d7dcde2f5f3bca09e89a1f6c8db5034c777261
amcclendon4 <amcclendon4@my.nl.edu>
2026-07-30 12:27:48 -0500

    Add metadata comments to time.py
    Added author, course, module, and description comments to the top of the file.

--- time.py
+++ time.py
@@ -1,3 +1,9 @@
+# Author: Alexandra McClendon
+# Course: CSS300
+# Module 4 Lab Activity
+# Description: Debugging corrections for this Python program.
+# This program calculates the future time after waiting a certain number of hours.
+
 current_time_str = input("What is the current time (in hours 0-23)? ")
 wait_time_str = input("How many hours do you want to wait? ")
 
