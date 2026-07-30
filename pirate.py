5585142f2a44773be46b777f3c9b6549d5e40a69
amcclendon4 <amcclendon4@my.nl.edu>
2026-07-30 12:27:02 -0500

    Add comments for author and program description
    Added author, course, and description comments to the file.

--- pirate.py
+++ pirate.py
@@ -1,3 +1,9 @@
+# Author: Alexandra McClendon
+# Course: CSS300
+# Module 4 Lab Activity
+# Description: Debugging corrections for this Python program.
+# This program checks a password and prints a different message for pirates and non pirates.
+
 greeting = input("Hello, possible pirate! What's the password? ")
 
 if greeting == "Arrr!":
