e044a813b15da6e02f16fe8ebfe7fc4cab95baed
Alexandra McClendon <amcclendon4@my.nl.edu>
2026-08-07 19:07:22 -0500

    Add files via upload

---
+++ numbers question 2.py
@@ -0,0 +1,11 @@
+# Alexandra McClendon
+# August 2026
+# This program prints numbers and their squares
+
+numbers = [12, 10, 32, 3, 66, 17, 42, 99, 20]
+
+for num in numbers:
+    print(num)
+
+for num in numbers:
+    print(num, num * num)
